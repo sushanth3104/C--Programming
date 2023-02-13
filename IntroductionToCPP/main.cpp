738fceb0b1280c9600e8b9a3b5a3ea2f7324cbe5
sushanth3104 <sushkumarthaviti@gmail.com>
2023-02-13 12:58:41 +0530

    Removed Default file and wrote introduction to CPP

---
+++ IntroductionToCPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  IntroductionToCPP
+//
+//  Created by Sushanth Kumar on 13/02/23.
+//
+
+#include<iostream>
+
+int main(){
+    
+    std::cout << "Hello Worlds !" << std::endl ;
+    return 0 ;
+}
